3cb97163038d2769aa0dbd989b333486894e4e44
jpmadridf <57046870+jpmadridf@users.noreply.github.com>
2021-10-21 13:18:05 -0500

    Update settings.py

--- MiUniversidad/MiUniversidad/settings.py
+++ MiUniversidad/MiUniversidad/settings.py
@@ -130,4 +130,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "juanpablo.madridflorez@aiesec.net"
-EMAIL_HOST_PASSWORD = "FYMEKEVI"
+EMAIL_HOST_PASSWORD = "********"
